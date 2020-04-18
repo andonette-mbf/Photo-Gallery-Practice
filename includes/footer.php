6ece1d4c4fa36c560113aca26e8f50987a6dfc22
andonette <andi@madebyfactory.com>
2020-04-18 17:26:11 +0100

    Basic Paper Kit & Dashboard

--- includes/footer.php
+++ includes/footer.php
@@ -1,51 +1,218 @@
-<footer class="footer footer-black">
+
+  <footer class="footer footer-big footer-black">
     <div class="container">
-        <div class="row">
-            <nav class="footer-nav">
-                <ul>
-                    <li><a href="http://www.creative-tim.com">Creative Tim</a></li>
-                    <li><a href="http://blog.creative-tim.com">Blog</a></li>
-                    <li><a href="https://www.creative-tim.com/license">Licenses</a></li>
-                </ul>
-            </nav>
-            <div class="credits ml-auto">
-                <span class="copyright">
-                    © <script>document.write(new Date().getFullYear())</script>, made with <i class="fa fa-heart heart"></i> by Creative Tim
-                </span>
-            </div>
+      <div class="row">
+        <div class="col-md-2 col-sm-3 col-6 ml-auto mr-auto">
+          <div class="form-group dropup">
+            <select name="huge" class="selectpicker" data-style="btn-default btn-block btn-round" data-menu-style="dropdown-blue">
+              <option value="id">Bahasa Indonesia</option>
+              <option value="ms">Bahasa Melayu</option>
+              <option value="ca">Català</option>
+              <option value="da">Dansk</option>
+              <option value="de">Deutsch</option>
+              <option value="en" selected="">English</option>
+              <option value="es">Español</option>
+              <option value="el">Eλληνικά</option>
+              <option value="fr">Français</option>
+              <option value="it">Italiano</option>
+              <option value="hu">Magyar</option>
+              <option value="nl">Nederlands</option>
+              <option value="no">Norsk</option>
+              <option value="pl">Polski</option>
+              <option value="pt">Português</option>
+              <option value="fi">Suomi</option>
+              <option value="sv">Svenska</option>
+              <option value="tr">Türkçe</option>
+              <option value="is">Íslenska</option>
+              <option value="cs">Čeština</option>
+              <option value="ru">Русский</option>
+              <option value="th">ภาษาไทย</option>
+              <option value="zh">中文 (简体)</option>
+              <option value="zh-TW">中文 (繁體)</option>
+              <option value="ja">日本語</option>
+              <option value="ko">한국어</option>
+            </select>
+          </div>
+          <div class="form-group dropup">
+            <select name="huge" class="selectpicker" data-style="btn-default btn-block btn-round" data-menu-style="dropdown-blue">
+              <option value="ARS">ARS</option>
+              <option value="AUD">AUD</option>
+              <option value="BRL">BRL</option>
+              <option value="CAD">CAD</option>
+              <option value="CHF">CHF</option>
+              <option value="CNY">CNY</option>
+              <option value="CZK">CZK</option>
+              <option value="DKK">DKK</option>
+              <option value="EUR">EUR</option>
+              <option value="GBP">GBP</option>
+              <option value="HKD">HKD</option>
+              <option value="HUF">HUF</option>
+              <option value="IDR">IDR</option>
+              <option value="ILS">ILS</option>
+              <option value="INR">INR</option>
+              <option value="JPY">JPY</option>
+              <option value="KRW">KRW</option>
+              <option value="MYR">MYR</option>
+              <option value="MXN">MXN</option>
+              <option value="NOK">NOK</option>
+              <option value="NZD">NZD</option>
+              <option value="PHP">PHP</option>
+              <option value="PLN">PLN</option>
+              <option value="RUB">RUB</option>
+              <option value="SEK">SEK</option>
+              <option value="SGD">SGD</option>
+              <option value="TWD">TWD</option>
+              <option value="USD" selected="">USD</option>
+              <option value="VND">VND</option>
+              <option value="ZAR">ZAR</option>
+            </select>
+          </div>
         </div>
+        <div class="col-md-9 ml-auto mr-auto col-sm-9 col-12">
+          <div class="row">
+            <div class="col-md-3 col-sm-3 col-6">
+              <div class="links">
+                <ul class="uppercase-links stacked-links">
+                  <li>
+                    <a href="#paper-kit">
+                      Home
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#paper-kit">
+                      Discover
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#paper-kit">
+                      Blog
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-md-3 col-sm-3 col-6">
+              <div class="links">
+                <ul class="uppercase-links stacked-links">
+                  <li>
+                    <a href="#paper-kit">
+                      Contact Us
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#paper-kit">
+                      We're Hiring
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#paper-kit">
+                      About Us
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-md-3 col-sm-3 col-6">
+              <div class="links">
+                <ul class="uppercase-links stacked-links">
+                  <li>
+                    <a href="#paper-kit">
+                      Portfolio
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#paper-kit">
+                      How it works
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#paper-kit">
+                      Testimonials
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-md-3 col-6">
+              <div class="social-area">
+                <a class="btn btn-just-icon btn-round btn-facebook">
+                  <i class="fa fa-facebook" aria-hidden="true"></i>
+                </a>
+                <a class="btn btn-just-icon btn-round btn-twitter">
+                  <i class="fa fa-twitter" aria-hidden="true"></i>
+                </a>
+                <a class="btn btn-just-icon btn-round btn-google">
+                  <i class="fa fa-google-plus" aria-hidden="true"></i>
+                </a>
+                <a class="btn btn-just-icon btn-round btn-pinterest">
+                  <i class="fa fa-pinterest-p" aria-hidden="true"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+          <hr />
+          <div class="copyright">
+            <div class="pull-left">
+              ©
+              <script>
+                document.write(new Date().getFullYear())
+              </script> Creative Tim, made with love
+            </div>
+            <div class="links pull-right">
+              <ul>
+                <li>
+                  <a href="#paper-kit">
+                    Company Policy
+                  </a>
+                </li>
+                |
+                <li>
+                  <a href="#paper-kit">
+                    Terms
+                  </a>
+                </li>
+                |
+                <li>
+                  <a href="#paper-kit">
+                    Privacy
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-</footer>
-
-<script src="js/core/jquery.min.js" type="text/javascript"></script>
-<script src="js/core/popper.min.js" type="text/javascript"></script>
-<script src="js/core/bootstrap.min.js" type="text/javascript"></script>
-
-
-<!-- Plugins -->
-
-<!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
-<script src="js/plugins/bootstrap-switch.js"></script>
-
-<!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
-<script src="js/plugins/nouislider.min.js"></script>
-
-<!--	Plugin for Select, full documentation here: http://silviomoreto.github.io/bootstrap-select -->
-<script src="js/plugins/bootstrap-selectpicker.js"></script>
-
-<!--	Plugin for Tags, full documentation here: https://github.com/bootstrap-tagsinput/bootstrap-tagsinputs  -->
-<script src="js/plugins/bootstrap-tagsinput.js"></script>
-
-<!--  Plugin for the DatePicker, full documentation here: https://github.com/uxsolutions/bootstrap-datepicker -->
-<!-- <script src="js/plugins/bootstrap-datetimepicker.js"></script> -->
-
-<!-- Photoswipe files -->
-
-<script src="js/plugins/photo_swipe/photoswipe.min.js"></script>
-<script src="js/plugins/photo_swipe/photoswipe-ui-default.min.js"></script>
-<script src="js/plugins/photo_swipe/init-gallery.js"></script>
-
-<!--  for Jasny fileupload -->
-<script src="js/plugins/jasny-bootstrap.min.js"></script>
+  </footer>
+  <!--   Core JS Files   -->
+  <script src="js/core/jquery.min.js" type="text/javascript"></script>
+  <script src="js/core/popper.min.js" type="text/javascript"></script>
+  <script src="js/core/bootstrap.min.js" type="text/javascript"></script>
+  <!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
+  <script src="js/plugins/bootstrap-switch.js"></script>
+  <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+  <script src="js/plugins/nouislider.min.js" type="text/javascript"></script>
+  <!--  Plugin for the DatePicker, full documentation here: https://github.com/uxsolutions/bootstrap-datepicker -->
+  <script src="js/plugins/moment.min.js"></script>
+  <!--	Plugin for Tags, full documentation here: https://github.com/bootstrap-tagsinput/bootstrap-tagsinputs  -->
+  <script src="js/plugins/bootstrap-tagsinput.js"></script>
+  <!--	Plugin for Select, full documentation here: http://silviomoreto.github.io/bootstrap-select -->
+  <script src="js/plugins/bootstrap-selectpicker.js" type="text/javascript"></script>
+  <!--	Plugin for Datetimepicker, full documentation here: https://eonasdan.github.io/bootstrap-datetimepicker/ -->
+  <script src="js/plugins/bootstrap-datetimepicker.js" type="text/javascript"></script>
+  <!--  Vertical nav - dots -->
+  <!--  Photoswipe files -->
+  <script src="js/plugins/photo_swipe/photoswipe.min.js"></script>
+  <script src="js/plugins/photo_swipe/photoswipe-ui-default.min.js"></script>
+  <script src="js/plugins/photo_swipe/init-gallery.js"></script>
+  <!--  for Jasny fileupload -->
+  <script src="js/plugins/jasny-bootstrap.min.js"></script>
+  <!-- Control Center for Paper Kit: parallax effects, scripts for the example pages etc -->
+  <script src="js/paper-kit.js?v=2.3.0" type="text/javascript"></script>
+  <!--  Google Maps Plugin    -->
+  <!-- <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script> -->
+  <!--  Plugin for presentation page - isometric cards  -->
+  <script src="js/plugins/presentation-page/main.js"></script>
 </body>
+
 </html>
--- includes/header.php
+++ includes/header.php
@@ -1,22 +1,25 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <title>Hello, world!</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css" integrity="" crossorigin="anonymous">
-
-    <!-- Paper Kit CSS -->
-    <link rel="stylesheet" href="css/paper-kit.css">
-
+    <meta charset="utf-8" />
+    <link rel="apple-touch-icon" sizes="76x76" href="../assets/img//apple-icon.png">
+    <link rel="icon" type="image/png" href="../assets/img//favicon.png">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <title>
+        Paper Kit 2 Pro by Creative Tim
+    </title>
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+    <!--     Fonts and icons     -->
     <!--     Fonts and icons     -->
     <link href='https://fonts.googleapis.com/css?family=Montserrat:400,300,700' rel='stylesheet' type='text/css'>
     <script src="https://kit.fontawesome.com/84146e01c9.js" crossorigin="anonymous"></script>
-
+    <!-- CSS Files -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link href="css/paper-kit.css?v=2.3.0" rel="stylesheet" />
 
 </head>
-<body>
+
+<body class="about-us sidebar-collapse">
+
     <?php include 'nav.php' ?>
